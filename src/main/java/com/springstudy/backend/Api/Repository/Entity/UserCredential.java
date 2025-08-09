23d38d5bc695e9d67f404dc0960bbd4b34113043
jhs0112 <gustjd9373@naver.com>
2025-08-09 19:47:32 +0900

    병합 완료

--- src/main/java/com/springstudy/backend/Api/Repository/Entity/UserCredential.java
+++ src/main/java/com/springstudy/backend/Api/Repository/Entity/UserCredential.java
@@ -8,14 +8,8 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 @Getter
-<<<<<<<< HEAD:src/main/java/com/springstudy/backend/Api/Repository/Entity/UserCredential.java
 @Table(name = "user_credentials")
 public class UserCredential {
-========
-@Table(name = "userCredentional")
-public class UserCredentional {
->>>>>>>> jhs2:src/main/java/com/springstudy/backend/Api/Repository/Entity/UserCredentional.java
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
