8bd2be09eeec754c55c585d874ff30ef2049a356
jhs0112 <gustjd9373@naver.com>
2025-08-09 19:37:20 +0900

    엔티티 인식 에러 수정

--- src/main/java/com/springstudy/backend/BackendApplication.java
+++ src/main/java/com/springstudy/backend/BackendApplication.java
@@ -3,9 +3,10 @@ package com.springstudy.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-
-@SpringBootApplication
-@EntityScan("com.springstudy.backend.Api.Repoitory.Entity")
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+@SpringBootApplication(scanBasePackages = "com.springstudy.backend")
+@EntityScan("com.springstudy.backend.Api.Repository.Entity")
+@EnableJpaRepositories("com.springstudy.backend.Api.Repository")
 public class BackendApplication {
 
     public static void main(String[] args) {
