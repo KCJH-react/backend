cf9cddbb557d86d8daded057b184463cf8828722
jhs0112 <gustjd9373@naver.com>
2025-08-31 06:39:43 +0900

    유저 아이디로 챌린지 생성 정보 받아오기 적용

--- src/main/java/com/springstudy/backend/Api/ChatGPT/Controller/ChatGPTController.java
+++ src/main/java/com/springstudy/backend/Api/ChatGPT/Controller/ChatGPTController.java
@@ -1,6 +1,8 @@
 package com.springstudy.backend.Api.ChatGPT.Controller;
 
 
+import com.springstudy.backend.Api.Auth.Model.UserDTO;
+import com.springstudy.backend.Api.Auth.Service.AuthService;
 import com.springstudy.backend.Api.Challenge.Model.Response.ChallengeResponse;
 import com.springstudy.backend.Api.ChatGPT.Prompt.PromptTemplate;
 import com.springstudy.backend.Api.ChatGPT.Service.ChatGPTService;
@@ -20,22 +22,32 @@ import java.util.Map;
 @RequestMapping("/api/chat")
 public class ChatGPTController {
     private final ChatGPTService chatService;
+    private final AuthService authService;
 
-    public ChatGPTController(ChatGPTService chatService) {
+    public ChatGPTController(ChatGPTService chatService, AuthService authService) {
         this.chatService = chatService;
+        this.authService = authService;
     }
 
-    //추후에 사용자의 기본 정보를 데이터베이스에서 가져와서 자동으로 프롬프트 완성하는 방식으로 바꿔야함
+    //성공률 계산 만들어야함
     @GetMapping("/challenge")
     public ResponseEntity<Response<ChallengeResponse>> getChallenge(
-            @RequestParam List<String> categories,
-            @RequestParam int age,
-            @RequestParam String sex,
             @RequestParam int successRate,
             @RequestParam Long userid
     ) {
-        String system_prompt = PromptTemplate.generateSystemPrompt(categories);
-        String user_prompt = PromptTemplate.generateUserPrompt(categories, age, sex, successRate);
+        ResponseEntity<Response<UserDTO>> userResponse = authService.get(userid);
+        UserDTO userDTO = userResponse.getBody().getData();
+
+        List<String> category = userDTO.getCategory()
+                .stream()
+                .map(Enum::name)
+                .toList();
+
+        int age = userDTO.getAge();
+        String sex = userDTO.getSex().name();
+
+        String system_prompt = PromptTemplate.generateSystemPrompt(category);
+        String user_prompt = PromptTemplate.generateUserPrompt(category, age, sex, successRate);
 
         return chatService.makeChallengeResult(system_prompt, user_prompt, userid);
     }
