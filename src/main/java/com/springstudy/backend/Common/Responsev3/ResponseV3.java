7c2372f046da3f4e523a9a9bf4527bab071f02a8
ghkdwnsdlr <imbanana15@gmail.com>
2025-09-16 17:55:28 +0900

    responsev3 생성2

--- src/main/java/com/springstudy/backend/Common/Responsev3/ResponseV3.java
+++ src/main/java/com/springstudy/backend/Common/Responsev3/ResponseV3.java
@@ -1,12 +1,15 @@
 package com.springstudy.backend.Common.Responsev3;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
-@Builder
+@AllArgsConstructor
 @Data
+@NoArgsConstructor
 public class ResponseV3<T> {
     private T data;
     private LocalDateTime timestamp;
